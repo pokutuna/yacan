69edd7a9b425ab1b73ef752465bce48f1c75235f
pokutuna <popopopopokutuna@gmail.com>
2011-04-30 22:54:22 +0900

    dependency

---
+++ lib/yacan/dependency.rb
@@ -0,0 +1,73 @@
+module YaCan::Dependency
+  @@path = '/DAService/V1/parse'
+
+  def analyze(text)
+    xml = YaCan::YahooAPI.request(@@path, { 'sentence' => text })
+    return Result.new(xml)
+  end
+
+  def parse(text)
+    analyze(text)
+  end
+
+  module_function :analyze, :parse
+
+  class Result
+    def initialize(xml)
+      @xml = xml
+      xml_chunks = Nokogiri::XML(xml).search('Chunk')
+      @chunks = xml_chunks.map{ |c| Chunk.new(c, lambda{ @chunks }) }
+      @morphems = chunks.map{|c| c.morphems}.flatten
+    end
+
+    attr_reader :xml, :chunks, :morphems
+
+    class Chunk
+      def initialize(xml, chunks)
+        @id = xml.at('Id').text.to_i
+        @dependency = xml.at('Dependency').text.to_i
+        @morphems = xml.search('Morphem').map{ |m| Morphem.new(m) }
+        @chunks = chunks
+      end
+
+      def to_s
+        "#{@id} #{dependency} #{@morphems.map{ |m| m.surface}.join('')}"
+      end
+
+      def depends_on
+        @depends_o ||= @chunks.call.find{ |c| c.id == @dependency }
+      end
+
+      def depends_from
+        @depends_f ||= @chunks.call.find_all{ |c| c.dependency == @id }
+      end
+
+      attr_reader :id, :dependency, :morphems
+    end
+
+
+    class Morphem
+      def initialize(xml)
+        @surface = xml.at('Surface').text
+        @reading = xml.at('Reading').text
+        @baseform = xml.at('Baseform').text
+        @pos = xml.at('POS').text
+        @feature = xml.at('Feature').text.split(',')
+      end
+
+      def to_s
+        "#{@surface}: #{@feature.join(',')}"
+      end
+
+      attr_reader :surface, :reading, :baseform, :pos, :feature
+    end
+
+  end
+
+
+end
+
+
+
+
+
---
+++ spec/dependency_spec.rb
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "Dependency" do
+  before(:all) do
+    YaCan.appid = ENV['YAHOO_API_APPID']
+    @phrase = 'うちの庭には二羽鶏が居ます。'
+    @xml = File.open(File.expand_path(File.dirname(__FILE__) + '/test_source/dependency.xml')).read
+  end
+
+  it 'should analyze sentence' do
+    YaCan::YahooAPI.stub!(:request).and_return @xml
+    res = YaCan::Dependency.analyze(@phrase)
+  end
+
+  it 'should have alias method :parse' do
+    YaCan::Dependency.parse(@phrase).chunks.to_s.should == YaCan::Dependency.analyze(@phrase).chunks.to_s
+  end
+
+  describe "Result" do
+    before(:all) do
+      @result = YaCan::Dependency::Result.new(@xml)
+    end
+
+    it 'should have chunks' do
+      @result.chunks.should have(4).items
+    end
+
+    it 'should be gettable a chunk from dependency' do
+      @result.chunks.first.depends_on.id.should == 1
+      @result.chunks.last.depends_on.should == nil
+    end
+
+    it 'should be gettable chunks from a chunk depends from' do
+      @result.chunks.last.depends_from.map{ |c| c.id }.should == [1,2]
+      @result.chunks.first.depends_from.should be_empty
+    end
+  end
+end
+
+
+
+
+
+
+
+
